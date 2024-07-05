77c97b8143e32c1222a7d7fd0c7f98523c15f5e3
Ken Fujii <44661517+kenfj@users.noreply.github.com>
2024-07-05 10:36:42 +0900

    add redux code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1>Redux Counter</h1>
-      </div>
+"use client";
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <h1>Zustand Counter</h1>
-      </div>
+import Link from "next/link";
+
+import { decrement, increment } from "@/lib/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+
+export default function Home() {
+  const { value } = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
+
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1>Redux Counter VS Zustand Counter</h1>
+
+      <h2>Redux Count: {value}</h2>
+      <button onClick={() => { dispatch(increment()); }}>UP: +</button>
+      <button onClick={() => { dispatch(decrement()); }}>DOWN: -</button>
+
+      <Link href="/zustand-counter">move to Zustand Counter</Link>
     </main>
   );
 }
